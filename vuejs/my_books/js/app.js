f888c8c35e0b505ed37270d3e4304b8e8343b78d
Paulo J. Matos <paulojomatos@gmail.com>
2016-06-26 19:49:45 -0400

    pagination and app completely done

--- vuejs/my_books/js/app.js
+++ vuejs/my_books/js/app.js
@@ -1,22 +1,77 @@
 var app = new Vue({
 	el: '#app',
 	data: {
+            title: 'Books',
             books: [],
             MySearch: '',
             orderCol: 'id',
-            orderInverse: 1
+            orderInverse: 1,
+            pagination: {
+                maxPage: 4,
+                current: 1,
+                totalItems: 10,
+                totalPages: 5,
+                listPagination: []
+            }
 	},
 	methods: {
-		filterOrderBy: function (event, col) {
+		filterOrderBy: function (e, col) {
+                    e.preventDefault();
                     this.orderCol = col;
                     this.orderInverse = this.orderInverse * -1;
-                    event.preventDefault();
+                },
+                previous: function (e) {
+                    e.preventDefault();
+                    
+                    if(this.pagination.current === 1) {
+                        return false;
+                    }
+                    else {
+                        this.pagination.current -= 1;
+                    }
+                    
+                    this.books = this.pagination.listPagination[this.pagination.current - 1];
+                },
+                pagePagination: function (e, current) {
+                    e.preventDefault();
+                    this.pagination.current = current + 1;
+                    this.books = this.pagination.listPagination[current];
+                },
+                next: function (e) {
+                    e.preventDefault();
+                    if(this.pagination.current === this.pagination.totalPages) {
+                        return false;
+                    }
+                    else {
+                        this.pagination.current += 1;
+                    }
+                    
+                    this.books = this.pagination.listPagination[this.pagination.current - 1];
                 }
 	},
         ready: function () {
-            var that = this;
-            that.$http.get('dataServer.json').then(function (response) {
-                that.books = response.data;
+            var self = this;
+            self.$http.get('dataServer.json').then(function (response) {
+                
+                
+                self.pagination.totalItems = response.data.length;
+                self.pagination.totalPages = Math.ceil( response.data.length / self.pagination.maxPage );
+                
+
+                var aux = [];
+                for(k in response.data) {
+                    aux.push(response.data[k]);
+                    if(aux.length === self.pagination.maxPage) {
+                        self.pagination.listPagination.push(aux);
+                        aux = [];
+                    }
+                }
+                if(aux.length) {
+                    self.pagination.listPagination.push(aux);
+                }
+                
+                self.books = self.pagination.listPagination[0];
+                
             });
         }
 });
